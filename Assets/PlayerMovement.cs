fc3e1e7b2e83405bc031bb22818d758f1fbc4bf4
Chingis Toktamyssov <coktamyssovthingis@gmail.com>
2024-11-23 19:46:24 -0500

    playermovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D body;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(xInput) > 0) {
+            body.linearVelocity = new Vector2(xInput * speed, body.linearVelocity.y);
+        }
+
+         if (Mathf.Abs(yInput) > 0) {
+            body.linearVelocity = new Vector2(body.linearVelocity.y, yInput * speed);
+        }
+
+        // Vector2 direction = new Vector2(xInput, yInput).normalized;
+        // body.velocity = direction * speed;
     }
 }
