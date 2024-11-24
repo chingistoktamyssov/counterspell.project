d036a7b402ce074e9b2227bde2087d1f3aff397c
Chingis Toktamyssov <coktamyssovthingis@gmail.com>
2024-11-24 04:17:32 -0500

    asdfasdfdas

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -28,13 +28,7 @@ public class PlayerMovement : MonoBehaviour
           Debug.Log("Pipe deleted");
           Destroy(gameObject); 
         }
-=======
-        // if(transform.position.x<deadZone){
-        //     Debug.Log("Pipe deleted");
-        //     Destroy(gameObject); 
-        // }
->>>>>>> 9d4f35d30da3dbd14c97157b373c245298829fa7
->>>>>>> 5167fdcf239b8357d9f3fd1041e0c42477a05673
+
     }
 
     void MoveWithInput() {
@@ -46,11 +40,9 @@ public class PlayerMovement : MonoBehaviour
             body.linearVelocity = Vector2.right*groundSpeed;
         }
 
-<<<<<<< HEAD
         if (Input.GetKeyDown(KeyCode.W) && grounded) {
-=======
+
         if (Input.GetKeyDown(KeyCode.W)) {
->>>>>>> 5167fdcf239b8357d9f3fd1041e0c42477a05673
             body.linearVelocity = Vector2.up*jumpSpeed;
         }
         float direction = Mathf.Sign(body.linearVelocity.x);
@@ -58,10 +50,6 @@ public class PlayerMovement : MonoBehaviour
     
     }
 
-    // void FaceInput() {
-    //     // float direction = Mathf.Sign(xInput);
-    //     // transform.localScale = new Vector3(direction, 1, 1);
-    // }
 
     void CheckGround() {
         grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
